3e67b261b13c1da5ccff795d9465ed6b769e4c8e
Qainguin <94211064+Qainguin@users.noreply.github.com>
2023-02-01 17:10:11 -0600

    tried to fix overflow on ios

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const left = document.getElementById("left-side");
+const $body = document.querySelector('body');
 
 const handleMove = e => {
   left.style.width = `${e.clientX / window.innerWidth * 100}%`;
@@ -7,3 +8,5 @@ const handleMove = e => {
 document.onmousemove = e => handleMove(e);
 
 document.ontouchmove = e => handleMove(e.touches[0]);
+
+$body.style.overflow = 'hidden';
